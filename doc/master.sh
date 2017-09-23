f0dbb0d566149969cdd9aab48a5a70bb534105f8
root <root@ubuntu.cloud>
2017-09-23 22:22:21 +0300

    master virtual machine script fix 2 (working)

--- doc/master.sh
+++ doc/master.sh
@@ -2,17 +2,20 @@
 
 apt-get update;
 
-apt-get install --no-install-recommends \
+apt-get remove docker docker-engine docker.io;
+
+apt-get update;
+
+apt-get install \
     apt-transport-https \
+    ca-certificates \
     curl \
     software-properties-common;
 
-curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e';
-
-apt-key add - ;
+curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | apt-key add - ;
 
 add-apt-repository \
-   "deb https://packages.docker.com/1.12/apt/repo/ \
+   "deb [arch=amd64] https://packages.docker.com/1.12/apt/repo/ \
    ubuntu-$(lsb_release -cs) \
    main" ;
 
@@ -27,9 +30,9 @@ aptitude install git make -y ;
 git clone https://github.com/yeasy/cello && cd cello ;
 
 docker pull python:3.5 \ 
-&& docker pull mongo:3.2 \
-&& docker pull yeasy/nginx:latest \
-&& docker pull mongo-express:0.30 ;
+ docker pull mongo:3.2 \
+ docker pull yeasy/nginx:latest \
+ docker pull mongo-express:0.30 ;
 
 apt install python-pip ;
 
@@ -41,4 +44,6 @@ make setup ;
 
 make redeploy service=dashboard ;
 
-make restart
+make restart;
+
+apt install w3m;
