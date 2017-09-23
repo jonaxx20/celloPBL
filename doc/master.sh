8b4a973133eb3b01ba77885689af62919daf8782
taku3717 <tautvydas.kubolis@mif.stud.vu.lt>
2017-09-23 21:31:53 +0300

    master vm script fix 1

--- doc/master.sh
+++ doc/master.sh
@@ -7,7 +7,9 @@ apt-get install --no-install-recommends \
     curl \
     software-properties-common;
 
-curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add - ;
+curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e';
+
+apt-key add - ;
 
 add-apt-repository \
    "deb https://packages.docker.com/1.12/apt/repo/ \
@@ -18,8 +20,6 @@ apt-get update ;
 
 apt-get install docker-ce ;
 
-docker run hello-world ;
-
 apt-get install aptitude ;
 
 aptitude install git make -y ;
@@ -27,9 +27,9 @@ aptitude install git make -y ;
 git clone https://github.com/yeasy/cello && cd cello ;
 
 docker pull python:3.5 \ 
-	&& docker pull mongo:3.2 \
-	&& docker pull yeasy/nginx:latest \
-	&& docker pull mongo-express:0.30 ;
+&& docker pull mongo:3.2 \
+&& docker pull yeasy/nginx:latest \
+&& docker pull mongo-express:0.30 ;
 
 apt install python-pip ;
 
